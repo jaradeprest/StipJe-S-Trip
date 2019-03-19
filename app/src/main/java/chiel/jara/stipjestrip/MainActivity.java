1e12c300dc833e7723191966a66aed9908015f7b
jaradeprest <jaradeprest@gmail.com>
2019-03-19 11:25:25 +0100

    walking man imported in launch screen

--- app/src/main/java/chiel/jara/stipjestrip/MainActivity.java
+++ app/src/main/java/chiel/jara/stipjestrip/MainActivity.java
@@ -2,12 +2,19 @@ package chiel.jara.stipjestrip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivGif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivGif = findViewById(R.id.iv_walkingman);
+        Glide.with(getApplicationContext()).load(R.drawable.walking_man).into(ivGif);
     }
 }
