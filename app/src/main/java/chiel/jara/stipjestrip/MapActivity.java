ebd380a224b27ff97e9c11da7eb7366dc03c2524
jaradeprest <jaradeprest@gmail.com>
2019-03-25 11:46:53 +0100

    added infoWindowAdapter to set the infowindow that you see when clicking on a marker in map. created layout for infoWindow (info_window_map)
    When you click on marker you see the image + name + author
    
    +cleanup in code

--- app/src/main/java/chiel/jara/stipjestrip/MapActivity.java
+++ app/src/main/java/chiel/jara/stipjestrip/MapActivity.java
@@ -15,13 +15,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,8 +45,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private final int REQUEST_LOCATION = 1; //constante variabele voor bij permissions
     private GoogleMap map;
-    private LayoutInflater inflater;
-    private Context context;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
