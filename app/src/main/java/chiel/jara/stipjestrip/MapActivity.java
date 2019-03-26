9bfd10ea4b000dfa1ae3a661c2c7eda91d6b0a18
chielbleyenbergh <chielbleynbergh@gmail.com>
2019-03-26 10:17:23 +0100

    automatich van launchscreen -> maps navigatie toegevoegd, navigeren naar striplijst!

--- app/src/main/java/chiel/jara/stipjestrip/MapActivity.java
+++ app/src/main/java/chiel/jara/stipjestrip/MapActivity.java
@@ -16,6 +16,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
