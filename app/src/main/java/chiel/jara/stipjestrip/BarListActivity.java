dd48d541387f49d9865c76fe1a2412892d445189
chielbleyenbergh <chielbleynbergh@gmail.com>
2019-03-27 11:04:48 +0100

    aanpassingen gedaan aan layout: -> launch scherm -> maps -> nav balk - lijsten (search balk)

--- app/src/main/java/chiel/jara/stipjestrip/BarListActivity.java
+++ app/src/main/java/chiel/jara/stipjestrip/BarListActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.widget.SearchView;
 
 import chiel.jara.stipjestrip.model.bar_model.BarDatabase;
 import chiel.jara.stipjestrip.util.bar_util.BarAdapter;
@@ -23,5 +25,13 @@ public class BarListActivity extends AppCompatActivity {
         rvBars.setAdapter(myBarAdapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         rvBars.setLayoutManager(linearLayoutManager);
+    }
+
+    //Aanmaken Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.mi_search).getActionView();
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/chiel/jara/stipjestrip/ListActivity.java
+++ app/src/main/java/chiel/jara/stipjestrip/ListActivity.java
@@ -3,9 +3,13 @@ package chiel.jara.stipjestrip;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import chiel.jara.stipjestrip.model.comic_model.ComicDatabase;
 import chiel.jara.stipjestrip.util.comic_util.ComicAdapter;
@@ -30,6 +34,14 @@ public class ListActivity extends AppCompatActivity {
         rvComics.setAdapter(myComicAdapter);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         rvComics.setLayoutManager(linearLayoutManager);
+    }
+
+    //Aanmaken Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.mi_search).getActionView();
+        return super.onCreateOptionsMenu(menu);
     }
 
 
