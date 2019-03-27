e7eb4d4275b527b8488f0a7d03b6cc492fd7f9c7
chielbleyenbergh <chielbleynbergh@gmail.com>
2019-03-27 11:09:38 +0100

    aanpassingen gedaan aan layout: -> launch scherm -> maps -> nav balk - lijsten (search balk)

--- app/src/main/java/chiel/jara/stipjestrip/ListActivity.java
+++ app/src/main/java/chiel/jara/stipjestrip/ListActivity.java
@@ -3,12 +3,10 @@ package chiel.jara.stipjestrip;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.SearchView;
 
 import chiel.jara.stipjestrip.model.comic_model.ComicDatabase;
